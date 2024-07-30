40f0d9d9a06513cd1723f77b93c7d9093c0e73f1
JRC <69986863+JRGC83@users.noreply.github.com>
2024-07-30 17:59:58 -0400

    Update dp_auxretry.cpp

--- src/common/displayport/src/dp_auxretry.cpp
+++ src/common/displayport/src/dp_auxretry.cpp
@@ -27,7 +27,6 @@
 *                                                                           *
 \***************************************************************************/
 
-
 #include "dp_internal.h"
 #include "dp_auxbus.h"
 #include "dp_auxretry.h"
@@ -37,6 +36,30 @@
 
 using namespace DisplayPort;
 
+namespace {
+    AuxRetry::status handleTransaction(AuxBus::status s, unsigned completed, unsigned size, unsigned &retries) {
+        if ((s == AuxBus::success) && (completed == size) && (completed != 0)) {
+            return AuxRetry::ack;
+        } else {
+            if (s == AuxBus::defer && retries > 0) {
+                --retries;
+                return AuxRetry::defer;
+            }
+            if (s == AuxBus::nack) {
+                return AuxRetry::nack;
+            }
+            if (completed == 0) {
+                return AuxRetry::unsupportedRegister;
+            }
+            if (completed < size && retries > 0) {
+                --retries;
+                return AuxRetry::defer;
+            }
+        }
+        return AuxRetry::defer;
+    }
+}
+
 AuxRetry::status AuxRetry::readTransaction(int address, NvU8 * buffer, unsigned size, unsigned retries) {
     unsigned completed;
     AuxBus::status s;
@@ -44,37 +67,13 @@ AuxRetry::status AuxRetry::readTransaction(int address, NvU8 * buffer, unsigned 
 
     do {
         s = aux->transaction(AuxBus::read, AuxBus::native, address, buffer, size, &completed);
-        if ((s == AuxBus::success) && (completed == size) && (completed != 0)) {
-            return ack;
-        } else {
-            if (s == AuxBus::defer) {
-                if (retries) {
-                    --retries;
-                    continue;
-                }
-                return defer;
-            }
-            if (s == AuxBus::nack) {
-                return nack;
-            }
-            if (completed == 0) {
-                return unsupportedRegister;
-            }
-            if (completed < size) {
-                if (retries) {
-                    --retries;
-                    continue;
-                } else {
-                    return defer;
-                }
-            }
+        AuxRetry::status status = handleTransaction(s, completed, size, retries);
+        if (status != AuxRetry::defer) {
+            return status;
         }
-    } while(retries);
+    } while(retries > 0);
 
-    if ((s == AuxBus::defer) || (completed < size)) {
-        return defer;
-    }
-    return ack;
+    return AuxRetry::defer;
 }
 
 AuxRetry::status AuxRetry::writeTransaction(int address, NvU8 * buffer, unsigned size, unsigned retries) {
@@ -84,44 +83,19 @@ AuxRetry::status AuxRetry::writeTransaction(int address, NvU8 * buffer, unsigned
 
     do {
         s = aux->transaction(AuxBus::write, AuxBus::native, address, buffer, size, &completed);
-        if ((s == AuxBus::success) && (completed == size) && (completed != 0)) {
-            return ack;
-        } else {
-            if (s == AuxBus::defer) {
-                if (retries) {
-                    --retries;
-                    continue;
-                }
-                return defer;
-            }
-            if (s == AuxBus::nack) {
-                return nack;
-            }
-            DP_ASSERT(s == AuxBus::success);
-            if (completed == 0) {
-                return unsupportedRegister;
-            }
-            if (completed < size) {
-                if (retries) {
-                    --retries;
-                    continue;
-                } else {
-                    return defer;
-                }
-            }
+        AuxRetry::status status = handleTransaction(s, completed, size, retries);
+        if (status != AuxRetry::defer) {
+            return status;
         }
-    } while(retries);
+    } while(retries > 0);
 
-    if ((s == AuxBus::defer) || (completed < size)) {
-        return defer;
-    }
-    return ack;
+    return AuxRetry::defer;
 }
 
 AuxRetry::status AuxRetry::read(int address, NvU8 * buffer, unsigned size, unsigned retries) {
     for (unsigned i = 0; i < size;) {
         int todo = DP_MIN(size - i, aux->transactionSize());
-        status s = readTransaction(address+i, buffer+i, todo, retries);
+        status s = readTransaction(address + i, buffer + i, todo, retries);
         if (s != ack) {
             return s;
         }
@@ -133,7 +107,7 @@ AuxRetry::status AuxRetry::read(int address, NvU8 * buffer, unsigned size, unsig
 AuxRetry::status AuxRetry::write(int address, NvU8 * buffer, unsigned size, unsigned retries) {
     for (unsigned i = 0; i < size;) {
         int todo = DP_MIN(size - i, aux->transactionSize());
-        status s = writeTransaction(address+i, buffer+i, todo, retries);
+        status s = writeTransaction(address + i, buffer + i, todo, retries);
         if (s != ack) {
             return s;
         }
@@ -146,26 +120,24 @@ AuxBus::status AuxLogger::transaction(Action action, Type type, int address, NvU
     AuxBus::status result = bus->transaction(action, type, address, buffer, sizeRequested, sizeCompleted);
     hint[0] = 0;
     if (result == success) {
-        if (type == native) {
-            if (address == NV_DPCD_MBOX_DOWN_REQ || address == NV_DPCD_MBOX_UP_REP || address == NV_DPCD_MBOX_DOWN_REP || address == NV_DPCD_MBOX_UP_REQ) {
-                unsigned len = *sizeCompleted;
-                Buffer storage(buffer, len);
-                BitStreamReader reader(&storage, 0, len*8);
-                MessageHeader header;
-                DisplayPort::decodeHeader(&reader, &header, Address(1));
-                Address::StringBuffer sb;
-                DP_USED(sb);
-                dpHexDump(&hex[0], sizeof(hex), buffer, header.headerSizeBits/8);
-                dpHexDump(&hex_body[0], sizeof(hex), buffer + header.headerSizeBits/8, len - header.headerSizeBits/8);
+        if (type == native && (address == NV_DPCD_MBOX_DOWN_REQ || address == NV_DPCD_MBOX_UP_REP || address == NV_DPCD_MBOX_DOWN_REP || address == NV_DPCD_MBOX_UP_REQ)) {
+            unsigned len = *sizeCompleted;
+            Buffer storage(buffer, len);
+            BitStreamReader reader(&storage, 0, len * 8);
+            MessageHeader header;
+            DisplayPort::decodeHeader(&reader, &header, Address(1));
+            Address::StringBuffer sb;
+            DP_USED(sb);
+            dpHexDump(&hex[0], sizeof(hex), buffer, header.headerSizeBits / 8);
+            dpHexDump(&hex_body[0], sizeof(hex), buffer + header.headerSizeBits / 8, len - header.headerSizeBits / 8);
 #if defined(_DEBUG) || defined(DEBUG)
-                const char * name = "";
-                if (header.isTransactionStart && action==write && len > header.headerSizeBits/8) {
-                    name = getRequestId(buffer[header.headerSizeBits/8]);
-                }
-                DP_PRINTF(DP_NOTICE, "DP-AUX> %s%s%s%s%04Xh hint(to:%s %s%s %s #%d) { %s| %s}", sizeRequested == *sizeCompleted ? "" : "INCOMPLETE ", getStatus(result), getAction(action), getType(type), address, header.address.toString(sb), header.isTransactionStart ? "S" : "", header.isTransactionEnd ? "E" : "", name, header.messageNumber, hex, hex_body);
-#endif
-                return result;
+            const char * name = "";
+            if (header.isTransactionStart && action == write && len > header.headerSizeBits / 8) {
+                name = getRequestId(buffer[header.headerSizeBits / 8]);
             }
+            DP_PRINTF(DP_NOTICE, "DP-AUX> %s%s%s%s%04Xh hint(to:%s %s%s %s #%d) { %s| %s}", sizeRequested == *sizeCompleted ? "" : "INCOMPLETE ", getStatus(result), getAction(action), getType(type), address, header.address.toString(sb), header.isTransactionStart ? "S" : "", header.isTransactionEnd ? "E" : "", name, header.messageNumber, hex, hex_body);
+#endif
+            return result;
         }
     } else {
         hex[0] = 0;
@@ -176,11 +148,5 @@ AuxBus::status AuxLogger::transaction(Action action, Type type, int address, NvU
 }
 
 AuxBus * DisplayPort::CreateAuxLogger(AuxBus * auxBus) {
-    return new AuxLogger(auxBus);
-}
-
-}
-
-AuxBus* DisplayPort::CreateAuxLogger(AuxBus* auxBus) {
     return new AuxLogger(auxBus);
 }
